5e7acf7cfba03260cd2ff68b4dd991a2e6ef67e3
Artur Dyshynski <tavebran@gmail.com>
2023-09-09 19:55:53 +0300

    fix: try to fix mailing

--- le-mur/assets/js/script.js
+++ le-mur/assets/js/script.js
@@ -67,29 +67,48 @@ document.addEventListener('DOMContentLoaded', function () {
   async function formSend(e) {
     e.preventDefault();
 
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
+    const error = formValidate();
+    const formData = new FormData(form);
+    const json = JSON.stringify(Object.fromEntries(formData));
 
 
     if (error === 0) {
-      let response = await fetch('../../sendmail.php', {
-        method: 'POST',
-        body: formData
+      $.ajax({
+        url: "../../sendmail.php",
+        type: "POST",
+        data: json,
+        contentType: "application/json",
+        success: function () {
+          alert('success');
+          // $('#formCallback').html("<p class='success'>Бронь успешно отправлена</p>");
+          form.reset();
+        },
+        error: function () {
+          alert('fail');
+          // $('#formCallback').html("<p class='fail'>Что-то пошло не так. Пожалуйста, попробуйте ещё раз позже.</p>");
+          form.reset();
+        }
       });
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        form.reset();
-      } else {
-        alert('Ошибка');
-      }
+
+      // const response = await fetch('../../sendmail.php', {
+      //   method: 'POST',
+      //   body: formData
+      // });
+
+      // if (response.ok) {
+      //   let result = await response.json();
+      //   alert(result.message);
+      //   form.reset();
+      // } else {
+      //   alert('Ошибка');
+      // }
+
     } else {
       alert("Заполните обязательные поля");
     }
   }
 
-  function formValidate(form) {
+  function formValidate() {
     let error = 0;
     let formReq = document.querySelectorAll('._req');
     let numberRadio = 0;
@@ -100,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function () {
         formAddError(input);
         error++;
       } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-        isRadioChecked()
+        isRadioChecked();
         formAddError(input);
         error++;
       } else if (input.getAttribute("type") === "radio") {
